e646884f262764baa58b19d057e5029ff5fe12b8
Shalin Mehta <shalinm.app@gmail.com>
2020-06-10 15:29:01 +0530

    Deleted mcd.sh; Made new-1.txt more readable; Uncommented code blocks in myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 # ECHO COMMAND
-# echo Hello World!
+echo Hello World!
 
 # VARIABLES
 # Uppercase by convention
 # Can include letters, numbers and underscores
-# NAME="Shalin"
-# echo "My name is $NAME"
+NAME="Shalin"
+echo "My name is $NAME"
 
 # USER INPUT
 # -p = prompt the user
-# read -p "Enter your favourite number: " NUMBER
-# echo "Your favourite number is $NUMBER"
+read -p "Enter your favourite number: " NUMBER
+echo "Your favourite number is $NUMBER"
 
 # IF-ELSE STATEMENTS
-# if [ "$NAME" == "Shalin" ]
-# then 
-#     echo "You are the owner of this Mac"
-# elif [ "$NAME" == "Sheetal" ]
-# then 
-#     echo "You are definitely the owner of this Mac"
-# else 
-#     echo "You are not the owner of this Mac"
-# fi 
+if [ "$NAME" == "Shalin" ]
+then 
+    echo "You are the owner of this Mac"
+elif [ "$NAME" == "Sheetal" ]
+then 
+    echo "You are definitely the owner of this Mac"
+else 
+    echo "You are not the owner of this Mac"
+fi 
 
 # COMPARISON OPERATORS
 # -gt = greater than
@@ -43,47 +43,45 @@
 # -w file = True if the file is writable
 # -x file = True if the file is executable
 
-# FILE="test.txt"
-# if [ -f "$FILE" ]
-# then 
-#     echo "$FILE is a file"
-# else
-#     echo "$FILE is not a file"
-# fi
+FILE="test.txt"
+if [ -f "$FILE" ]
+then 
+    echo "$FILE is a file"
+else
+    echo "$FILE is not a file"
+fi
 
 # CASE STATEMENT
-# read -p "Are you 21 or over? Y/N " ANSWER
-# case "$ANSWER" in 
-#     [yY] | [yY][eE][sS])
-#         echo "You can have a beer :)"
-#         ;;
-#     [nN] | [nN][oO])
-#         echo "Sorry, no beer for you"
-#         ;;
-#     *)
-#         echo "Please enter y/yes or n/no"
-#         ;;
-# esac
+read -p "Are you 21 or over? Y/N " ANSWER
+case "$ANSWER" in 
+    [yY] | [yY][eE][sS])
+        echo "You can have a beer :)" ;;
+    [nN] | [nN][oO])
+        echo "Sorry, no beer for you" ;;
+    *)
+        echo "Please enter y/yes or n/no" ;;
+esac
 
 # FOR LOOPS
-# NAMES="Shalin Shimoli"
-# for NAME in $NAMES 
-#     do 
-#         echo "Hello $NAME"
-# done
+NAMES="Shalin Shimoli"
+for NAME in $NAMES 
+    do 
+        echo "Hello $NAME"
+done
 
-# START=1
-# END=5
-# echo "Countdown..."
-# for (( c=$START; c<=$END; c++ ))
-#     do 
-#         echo -n "$c "
-#         sleep 1
-# done
-# echo
-# echo "Boom!"
+START=1
+END=5
+echo "Countdown..."
+for (( i=$START; i<=$END; i++ ))
+    do 
+        echo -n "$i "
+        sleep 1
+done
+echo
+echo "Boom!"
 
 # FOR LOOP TO RENAME FILES
+# Using variable substitution
 # FILES=$(ls ?.txt)
 # NEW="new"
 # for FILE in $FILES
@@ -93,37 +91,37 @@
 # done
 
 # WHILE LOOP - READ FILE LINE BY LINE
-# LINE=1
-# while read -r CURRENT_LINE
-#     do
-#         echo "$LINE: $CURRENT_LINE"
-#         ((LINE++))
-# done < "./new-1.txt"
+LINE=1
+while read -r CURRENT_LINE
+    do
+        echo "$LINE: $CURRENT_LINE"
+        (( LINE++ ))
+done < "./new-1.txt"
 
-# N=1
-# while [ $N -le 10 ]
-#      do
-#         echo "$N"
-#         N=$(( N+1 ))
-# done
+N=1
+while [ $N -le 10 ]
+     do
+        echo "$N"
+        (( N++ ))
+done
 
 # FUNCTION
-# function greet() {
-#     echo "Hello! I am $1 and I am $2"
-# }
+function greet() {
+    echo "Hello! I am $1 and I am $2"
+}
 
-# greet "Shalin" "21"
+greet "Shalin" "21"
 
 # PARAMETER ALIASES
+# $$ = process id on your computer
 # $0 = script name
 # $1-$9 = first nine parameters for a command/program
 # $? = error message from previous command
 # $_ = last parameter from previous command
 # $# = number of parameters the program expects/can take
-# $$ = process id on your computer
 # $@ = list of the parameters
 
-# echo $? for true = 0 and for false = 1
+# echo $? for no error = 0 and for error = 1
 # || = or operator and && = and operator
 
 # VARIABLE SUBSTITUTION
@@ -149,5 +147,10 @@
 # tldr tool for better manual pages for commands with examples
 
 # ARRAYS
-# args=("$@")
-# echo ${args[0]} ${args[1]} ${args[2]}
+args=("$@")
+echo ${args[0]} ${args[1]} ${args[2]}
+
+# STDOUT AND STDERR
+# ls -al > file.txt (will only output the STDOUT into file.txt)
+# ls -al > file.txt 2>&1 === ls -al >& file.txt (will direct both STDOUT and STDERR into file.txt)
+# ls -al 1> file1.txt 2> file2.txt (similar to previous command but different syntax)
